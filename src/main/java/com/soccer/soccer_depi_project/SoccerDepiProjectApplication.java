39c1e67bd9cf8d6dec37fb64c1850ffbfdeff26a
julia <202227099@std.sci.cu.edu.eg>
2025-08-11 16:47:27 +0300

    Initial commit

---
+++ src/main/java/com/soccer/soccer_depi_project/SoccerDepiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.soccer.soccer_depi_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerDepiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoccerDepiProjectApplication.class, args);
+	}
+
+}
